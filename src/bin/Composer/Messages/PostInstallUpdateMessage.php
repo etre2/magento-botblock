58c0619b42c5160c7853030a8e81f49eddb126a0
Tyler Mills <tmills@monin.com>
2017-03-22 16:49:06 -0400

    Add botblock post install/post update message

--- src/bin/Composer/Messages/PostInstallUpdateMessage.php
+++ src/bin/Composer/Messages/PostInstallUpdateMessage.php
@@ -6,8 +6,9 @@ class PostInstallUpdateMessage
 {
     protected static $setupInfoMessages = [
         "\t".'<info>etre2/magento-botblock is installed.</info>',
-        "\t".'<comment>Note: In order for the self-update mechanism to work, make sure the domains/blocked file is writable by the webserver user:</comment>',
-        "\t    ".'chmod a+w vendor/nabble/semalt-blocker/domains/blocked'
+        "\t".'<comment> - In order for the self-update mechanism to work, make sure the domains/blocked file is writable by the webserver user:</comment>',
+        "\t    ".'chmod a+w vendor/nabble/semalt-blocker/domains/blocked',
+        "\t".'<comment> - The self-update mechanism updates the blocked domains list every 7 days based on the file\'s last updated timestamp. No cron job needed to automate this process.</comment>',
     ];
 
     public static function setupInfo()
